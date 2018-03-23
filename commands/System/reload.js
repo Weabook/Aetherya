61f0c720780b7c5b6bbded274f35e44bc2d13d33
OGNova <jamooney2000@gmail.com>
2018-03-23 11:16:01 -0400

    Updates.

--- commands/System/reload.js
+++ commands/System/reload.js
@@ -13,37 +13,18 @@ class Reload extends Command {
   }
 
   async run(message, [piece], level) {
-    // if (!args || args.size < 1) return message.reply('Must provide a command to reload. Derp.');
+    if (!args || args.size < 1) return message.reply('Must provide a command to reload. Derp.');
     
-    // const commands = this.client.commands.get(args[0]) || this.client.commands.get(this.client.aliases.get(args[0]));
-    // if (!commands) return message.reply(`The command \`${args[0]}\` doesn't seem to exist, nor is it an alias. Try again!`);
+    const commands = this.client.commands.get(args[0]) || this.client.commands.get(this.client.aliases.get(args[0]));
+    if (!commands) return message.reply(`The command \`${args[0]}\` doesn't seem to exist, nor is it an alias. Try again!`);
     
-    // let response = await this.client.unloadCommand(`${commands.conf.location}`, commands.help.name);
-    // if (response) return message.reply(`Error Unloading: ${response}`);
+    let response = await this.client.unloadCommand(`${commands.conf.location}`, commands.help.name);
+    if (response) return message.reply(`Error Unloading: ${response}`);
     
-    // response = this.client.loadCommand(`${commands.conf.location}`, commands.help.name);
-    // if (response) return message.reply(`Error loading: ${response}`);
+    response = this.client.loadCommand(`${commands.conf.location}`, commands.help.name);
+    if (response) return message.reply(`Error loading: ${response}`);
     
-    // message.reply(`The command \`${commands.help.name}\` has been reloaded.`);
-
-    if (!piece || !piece.length) return message.reply('Must provide a piece to reload. Derp.');
-    piece = this.resolvePiece(piece);
-    if (!piece) return message.reply('That is not a valid piece.');
-    try {
-      const reloadedPiece = piece.reload();
-      return message.channel.send(`I have successfully reloaded the ${piece} piece.`);
-    } catch (err) {
-      piece.store.set(piece);
-      return message.channel.send(`I was unable to reload the ${piece} piece`);
-    }
-  }
-
-  resolvePiece(arg) {
-    const isCommand = this.client.commands.get(arg);
-    if (isCommand) return isCommand;
-    const isEvent = this.client.events.get(arg);
-    if (isEvent) return isEvent;
-    return false;
+    message.reply(`The command \`${commands.help.name}\` has been reloaded.`);
   }
 }
 module.exports = Reload;
