d64576152156562324f13b133b977ae7be424a23
OGNova <jamooney2000@gmail.com>
2018-05-18 23:59:43 -0400

    Change bash script, update reload command.

--- .docker/startloop.sh
+++ .docker/startloop.sh
@@ -12,8 +12,6 @@ do
     npm start;
     export npmExitCode=$?;
     test $npmExitCode -eq 0 && break;
-    test $(expr $(date +%s) - 20) -lt $firstNpmStartTime && echo "died unreasonably quickly, propagating." && exit $npmExitCode;
-    test $(expr $(date +%s) - 90) -lt $lastNpmStartTime && echo "died too quickly, extending wait by 60 seconds.." && sleep 60;
     echo "died for unknown reason; waiting 10 seconds before restarting..";
     sleep 10;
 done
