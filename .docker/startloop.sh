3867ffbdcf06b3c0cfdbd233f6a783e27f41e234
OGNova <jamooney2000@gmail.com>
2018-07-03 19:41:01 -0400

    Docker apparently can't work with CRLF. Fun.

--- .docker/startloop.sh
+++ .docker/startloop.sh
@@ -1,19 +1,19 @@
 
 for waitstep in {5..1}
 do
-    echo "waiting to catch up ($waitstep seconds)..";
-    sleep 1;
+  echo "waiting for db to catch up ($waitstep seconds)..";
+  sleep 1;
 done
 
 export firstNpmStartTime=$(date +%s);
 while [ 1 ];
 do
-    export lastNpmStartTime=$(date +%s);
-    npm start;
-    export npmExitCode=$?;
-    test $npmExitCode -eq 0 && break;
-    echo "died for unknown reason; waiting 10 seconds before restarting..";
-    sleep 10;
-done
+  export lastNpmStartTime=$(date +%s);
+  npm start;
+  export npmExitCode=$?;
+  test $npmExitCode -eq 0 && break;
+  echo "died for unknown reason; waiting 10 seonds before restarting";
+  sleep 10;
+done;
 
-echo "died by either interrupt or standard exit; not restarting";
+echo "died either by interrupt or standard exit; not restarting";
